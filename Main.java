d4ee173a61b3fa151488043e219aa016d0fa66d1
Lightningeek <117481654+Lightningeek@users.noreply.github.com>
2023-05-19 11:24:06 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,13 @@
+public class Main {
 
+	public static void main(String[] args) {
+		System.out.println("Hello nerd.");
+		
+		var x = new Main();
+		x.tacos();
+		
+	}
+	public void tacos()     {
+		System.out.println("Hello there nerd tacos!");
+	}
+}
