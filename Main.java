0f736c3f2be9e29a406b26e1500fb4359fe236f0
Lightningeek <117481654+Lightningeek@users.noreply.github.com>
2023-05-23 18:48:07 +0000

    max is a nerd!!!!

--- Main.java
+++ Main.java
@@ -1,13 +1,29 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello nerd.");
+		System.out.println("Hello " + "nerd");
 		
 		var x = new Main();
 		x.tacos();
-		
+		var y = new Main();
+		y.maxnerd();
+		y.maxnerd();
+		y.maxnerd();
+		y.maxnerd();
+		y.maxnerd();
+		y.maxnerd();
+		y.maxnerd();
+		y.maxnerd();
+		y.maxnerd();
+		y.maxnerd();
+		y.maxnerd();
+		y.maxnerd();
+		y.maxnerd();
 	}
 	public void tacos()     {
 		System.out.println("Hello there nerd tacos!");
+	}
+	public void maxnerd() { //Max rly be a nerd ngl ngl
+		System.out.println("max is such a nerd");
 	}
 }
