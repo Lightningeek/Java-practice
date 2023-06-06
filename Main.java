0e523ad4968a1bc293d9f8d06653f78c6d0cd4c0
Lightningeek <117481654+Lightningeek@users.noreply.github.com>
2023-06-06 19:58:55 +0000

    Sussy max is a nerd

--- Main.java
+++ Main.java
@@ -1,29 +1,13 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello " + "nerd");
+		System.out.println("Hello nerd.");
 		
 		var x = new Main();
-		x.tacos();
-		var y = new Main();
-		y.maxnerd();
-		y.maxnerd();
-		y.maxnerd();
-		y.maxnerd();
-		y.maxnerd();
-		y.maxnerd();
-		y.maxnerd();
-		y.maxnerd();
-		y.maxnerd();
-		y.maxnerd();
-		y.maxnerd();
-		y.maxnerd();
-		y.maxnerd();
+		x.nerds();
+		
 	}
-	public void tacos()     {
-		System.out.println("Hello there nerd tacos!");
-	}
-	public void maxnerd() { //Max rly be a nerd ngl ngl
-		System.out.println("max is such a nerd");
+	public void nerds()  {
+		System.out.println("Max you are a NERD!");
 	}
 }
